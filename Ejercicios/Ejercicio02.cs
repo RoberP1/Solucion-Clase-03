661bf4606082094df53a00b26b117c5c0e0bdb5c
RoberP1 <roberpapeschi@gmail.com>
2022-09-13 14:38:13 -0300

    Update Ejercicio02.cs

--- Ejercicios/Ejercicio02.cs
+++ Ejercicios/Ejercicio02.cs
@@ -4,29 +4,22 @@ using UnityEngine;
 
 public class Ejercicio02 : MonoBehaviour
 {
-    /*---- Mejorá el código  ----*/
-
-    //Dato:no se que es,pero siento que algo no esta bien.
-
-
     int fuerza = 100;
     int agilidad = 50;
 
-
-    private void Start()
-    {
-        
-    }
-    
-    private void Update()
-    {
-
-    }
+    int fuerzaNecesaria = -1;
+    int agilidadNecesaria = 20;
 
     private void EjecutarAtaqueEspecial()
     {
-        if ((fuerza * 2) / 5 > -1 && (agilidad / 2 > 20))
+        float multiplicadorFuerza = 2 / 5;
+        float multiplicadorAgilidad = 1 / 2;
 
+        bool fuerzaSuficiente = fuerza * multiplicadorFuerza < fuerzaNecesaria;
+
+        bool agilidadSuficiente = agilidad * multiplicadorAgilidad < agilidadNecesaria;
+
+        if (fuerzaSuficiente && agilidadSuficiente)
         {
             Debug.Log("Ataque especial ejecutado");
         }
