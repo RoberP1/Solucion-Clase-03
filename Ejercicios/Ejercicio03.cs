46648491c8638162a5a02e3669aad185aa50c37a
RoberP1 <roberpapeschi@gmail.com>
2022-09-13 15:00:59 -0300

    Update Ejercicio03.cs

--- Ejercicios/Ejercicio03.cs
+++ Ejercicios/Ejercicio03.cs
@@ -4,54 +4,57 @@ using UnityEngine;
 
 public class Ejercicio03 : MonoBehaviour
 {
-    /*---- Ordena el Códgo  ----*/
+    private Rigidbody2D rb;
 
-        public float _Speed;
-        bool esta_Saltando;
-        public float Z;
-        private Rigidbody2D fisicas;
-        public _Speed2;
+    bool isJumping;
 
-        private void Start()
-        {
-            fisicas = GetComponent<Rigidbody2D>();
-        }
-        private void Update()
-        {
-            Movimiento();
-        }
+    public float jumpForce;
 
-        public void Movimiento()
-        {
-            //Movimiento del jugador - Der
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                float movimiento_x = Input.GetAxis("Horizontal");
-                fisicas.velocity = new Vector2(x, 0);
-            }
-            //Movimiento del jugador - Izquierda
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                float movimiento_x = Input.GetAxis("Horizontal");
-                fisicas.velocity = new Vector2(x, 0);
-            }
-            //Rotacion del jugador
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                Z += Time.deltaTime * 10;
-                transform.rotation = Quaternion.Euler(0, 0, Z);
+    public float speed;
 
-            }
-            //Salto del jugador 
-            if (Input.GetButton("Jump") && !esta_Saltando)
-            {
-                //Le aplico la fuerza de salto
-                rb2d.AddForce(Vector2.up * _Speed2;
-                Debug.Log("Estoy saltando Wiiii");
-                //Digo que está saltando (para que no pueda volver a saltar)
-                isJumping = true;
-            }
+    public float rotation;
+
+    public float rotationSpeed = 10;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        Movement();
     }
 
-    //....
+    public void Movement()
+    {
+        //Movimiento del jugador - Derecha
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+
+        //Movimiento del jugador - Izquierda
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        //Rotacion del jugador
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            rotation += Time.deltaTime * rotationSpeed;
+            transform.rotation = Quaternion.Euler(0, 0, rotation):
+        }
+
+        //Salto del jugador 
+        if (Input.GetButton("Jump") && !isJumping)
+        {
+            rb2d.AddForce(Vector2.up * jumpForce);
+            Debug.Log("Estoy saltando Wiiii");
+            isJumping = true;
+        }
+    }
+
+    //.... Función para resetear el salto
 }
